a9b59fe8ab28ffa6a87539af401190bfcef5bfaa
zhanglvmeng <itbird2010@163.com>
2019-11-25 10:11:57 +0800

    提交

---
+++ src/app.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">{{text}}
+    <test :msg="msg" @change="changeMessage"></test>
+    </div>
+</template>
+
+<script>
+    import Test from './test.vue'
+    export default {
+        components:{Test},
+        data() {
+            return {
+                text: 'vue索testss',
+                msg:"vue"
+            }
+        },
+        methods: {
+            changeMessage(val) {
+                console.log(val);
+                this.msg = val;
+            }
+        }
+    }
+</script>
---
+++ src/test.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <span @click="changeMsg">{{msg}}</span>
+        <div>{{message}}</div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        props: {
+            msg:"thisdsd"
+        },
+        data() {
+           return {
+               message:'this is vue'
+           }
+        },
+        methods: {
+            changeMsg(){
+                this.$emit('change','ling')
+            }
+        }
+    }
+</script>
